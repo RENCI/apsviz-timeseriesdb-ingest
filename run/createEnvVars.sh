44fee35ca875f86aa2e5404d2f2bb4ce834d3745
James McManus <jmpmcman@renci.org>
2023-01-27 15:19:41 -0500

    Revised

--- run/createEnvVars.sh
+++ run/createEnvVars.sh
@@ -1,8 +1,6 @@
-export DJANGO_ALLOWED_HOSTS=localhost 0.0.0.0 127.0.0.1 apsviz-timeseriesdb_db_1 [::1]
 export SQL_ENGINE=timescale.db.backends.postgis
 export SQL_DATABASE=apsviz_gauges
 export SQL_USER=apsviz_ingester
 export SQL_PASSWORD=xxxxxxxx
 export SQL_HOST=apsviz-timeseriesdb_db_1
 export SQL_PORT=5432
-export DATABASE=postgres
