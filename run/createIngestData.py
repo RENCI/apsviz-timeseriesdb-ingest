f56a3b048368c08e541aa4a07ff2ae31fc10fbce
James McManus <jmpmcman@renci.org>
2023-02-28 16:39:12 -0500

    Removed choices for inputDataSource

--- run/createIngestData.py
+++ run/createIngestData.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
 
     # Optional argument which requires a parameter (eg. -d test)
     parser.add_argument("--ingestDIR", "--ingestDir", help="Output directory path", action="store", dest="ingestDir", required=True)
-    parser.add_argument("--inputDataSource", help="Input data source name", action="store", dest="inputDataSource", choices=['namforecast_hsofs','namforecast_ec95d','nowcast_hsofs','nowcast_ec95d','tidal_gauge','tidal_predictions', 'air_barometer','ocean_buoy','wind_anemometer','coastal_gauge','river_gauge'], required=True)
+    parser.add_argument("--inputDataSource", help="Input data source name", action="store", dest="inputDataSource", required=True)
     parser.add_argument("--inputSourceName", help="Input source name", action="store", dest="inputSourceName", choices=['adcirc','noaa','ndbc','ncem'], required=True)
     parser.add_argument("--inputSourceArchive", help="Input source archive name", action="store", dest="inputSourceArchive", choices=['noaa','ndbc','contrails','renci'], required=True)
 
