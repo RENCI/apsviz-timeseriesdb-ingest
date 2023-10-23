f547f0c7cd5a52150fadf8d988355a7974c94885
James McManus <jmpmcman@renci.org>
2023-10-23 10:48:45 -0400

    Removed wind speed and air pressure

--- scripts/get_obs_timeseries_station_data_allparms.sql
+++ scripts/get_obs_timeseries_station_data_allparms.sql
@@ -12,7 +12,11 @@ $$
                     CAST(d.time AS TEXT) AS id,
                     s.data_source AS category,
                     COALESCE(d.water_level,
-                    d.wave_height) AS yaxis
+                    d.stream_elevation,
+                    d.wave_height,
+                    d.wind_speed,
+                    d.air_pressure,
+                    d.flow_volume) AS yaxis
                 FROM
                    drf_gauge_data d
                 JOIN
@@ -23,20 +27,26 @@ $$
                    g.station_name= ''''' || _station_name || ''''' AND
                    time >= ''''' || _start_date || ''''' AND time <= ''''' || _end_date || '''''
                 ORDER BY d.time'',
-                ''SELECT data_source FROM (VALUES (''''' || _nowcast_source || '''''),
+                ''SELECT data_source FROM (VALUES (''''air_barometer''''),
+                                                  (''''' || _nowcast_source || '''''),
                                                   (''''ocean_buoy''''),
                                                   (''''tidal_gauge''''),
                                                   (''''tidal_predictions''''),
                                                   (''''coastal_gauge''''),
-                                                  (''''river_gauge'''')) b(data_source)''
+                                                  (''''river_gauge''''),
+                                                  (''''stream_gauge''''), 
+                                                  (''''wind_anemometer'''')) b(data_source)''
                 ) AS (
                  time_stamp TEXT,
+                 "air_barometer" double precision,
                  ' || SPLIT_PART(_nowcast_source::TEXT, '.', 1) || SPLIT_PART(_nowcast_source::TEXT, '.', 2) || ' double precision,
                  "ocean_buoy_wave_height" double precision,
                  "tidal_gauge_water_level" double precision,
                  "tidal_predictions" double precision,
                  "coastal_gauge_water_level" double precision,
-                 "river_gauge_water_level" double precision
+                 "river_gauge_water_level" double precision,
+                 "stream_gauge_stream_elevation" double precision,
+                 "wind_anemometer" double precision
                 )) AS ct';
 BEGIN
     -- gather the records and return them in json format
